68a6241c1594d5f3745b29dce84d4ca6b7c892a7
Nils-Kristian Pedersen <nikrpedev@gmail.com>
2023-09-07 21:14:20 +0200

    add todolist logic

--- app/components/TodoList.tsx
+++ app/components/TodoList.tsx
@@ -1,15 +1,29 @@
 import React from 'react'
 import fetchTodos from '@/lib/fetchTodos'
+import Todo from './Todo'
 
 const TodoList = async () => {
 
   const todos = await fetchTodos()
+
+  let content
+
+  if(!todos || todos.length === 0) {
+
+    content = ( <p>Sorry, no todos available..</p>)
   
-  console.log('todos fromssssa wwk todolist', todos)
-  
-  return (
-    <div>TodoList</div>
-  )
+  } else {
+    
+    const sortedTodos = todos.reverse()
+    
+    content = (
+      <>
+      {sortedTodos.map((todo) => ( <Todo key={todo.id} {...todo}/>))}      
+      </>
+    )
+  }
+  return content
 }
+
 
 export default TodoList
--- lib/fetchTodos.ts
+++ lib/fetchTodos.ts
@@ -2,8 +2,6 @@ import React from 'react'
 
 
 
-
-
 const fetchTodos = async () => {
     const res = await fetch("http://localhost:3500/todos", { next: { revalidate: 0 }})
 
