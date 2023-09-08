de1e86a813624f698a9136b92043338d5c38d83c
Nils-Kristian Pedersen <nikrpedev@gmail.com>
2023-09-08 11:02:15 +0200

    add use server

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {}
-
-
-module.exports = {
+const nextConfig = {
     experimental: {
         serverActions: true,
     },
 }
+
 
 
 
