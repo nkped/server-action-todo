b6d648a03ddca1c9b1d9d607ffa12afc0abe1fa1
Nils-Kristian Pedersen <nikrpedev@gmail.com>
2023-09-11 15:19:09 +0200

    add useoptimistic

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'next/link'
+
+
+const Navbar = () => {
+  return (
+    <ul><li><Link href='/'>Go Home</Link></li></ul>
+  )
+}
+
+export default Navbar
--- app/components/UpdateCheckbox.tsx
+++ app/components/UpdateCheckbox.tsx
@@ -2,26 +2,37 @@
 
 import React from 'react'
 import { updateTodo } from '@/lib/actions'
-import { useTransition } from 'react'
+//import { useTransition } from 'react'
+import { experimental_useFormStatus as useformStatus } from 'react-dom'
+import { useRouter } from 'next/navigation'
+import { experimental_useOptimistic as useOptimistic } from 'react'
 
 
 const UpdateCheckbox = ({todo,}: {todo: Todo,}) => {
 
-  console.log('this is todo: ', todo)
-    const [ isPending, startTransition] = useTransition()
-
     console.log('this is todo: ', todo)
+    //const [ isPending, startTransition] = useTransition()
+    const { pending } = useformStatus()    
+    const router = useRouter()
+    const [ optimisticTodo, addOptimisticTodo ] = useOptimistic(todo, (
+      state: Todo,
+      completed: boolean
+    ) => ({...state, completed })
+    )
 
   return (
     <input
       type="checkbox"
       
-      checked={todo.completed}
+      checked={optimisticTodo.completed}
       id="completed"
       name="completed"
-      onChange={() => startTransition(() => updateTodo(todo))}
-      
-      disabled={isPending}
+  //    onChange={() => startTransition(() => updateTodo(todo))}
+      onChange={async () =>{ 
+        addOptimisticTodo(!todo.completed)
+        await updateTodo(todo)
+        router.refresh()}}
+        disabled={pending}
     
     />
   )
